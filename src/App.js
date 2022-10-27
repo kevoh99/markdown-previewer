5cc7037e26f70346bc7ef75cead356eb1c127c7c
Kevin Njoroge <njorogekevin.njoroge@gmail.com>
2022-10-28 02:01:29 +0300

    Refactored css a bit

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ marked.setOptions({
 })
 
 const defaultInput =
-  '# Heading 1\n## Heading 2\n### Heading 3\n#### And so on...\n##### Just change the No. of hash (#) symbols\n\nHere is an _Italicized text_\n\nAnd some **bold text** just because why not! This __also works__\n\nAnd oh yeah!! We can also ~~strikethrough some text~~\n\nWe can have some code: `<div>Hello</div>`\n\nA multiline code can also suffice:\n```\nconst handleClick = () => {\n\tconsole.log(\'clicked\');\n};\n\n```\nThis is how I would [link](https://www.facebook.com/kevin.njoroge) to my Facebook page\n\nIf you are like me, an inspiring quote can uplift a dull moment. Like this one I have picked from my [Quotes App](https://chipper-bubblegum-fb43cd.netlify.app):\n\n> "Whether you think you can or think you can\'t, you\'re right." - Henry Ford.\n\nVisit the [app](https://chipper-bubblegum-fb43cd.netlify.app) to enjoy some more quotes\n\nAnd lastly, before I forgot, this page is almost getting boring for its lack of an image.\n\nSo, here is one:\n\n![Sample image](https://picsum.photos/id/1024/960/640)\n\n## In Summary\n\nLet\'s end with a recap of what you can do in the \'Editor\' section to see real-time results in the \'Preview\' section:\n - Use # to define headings\n - Enclose some text in a pair of underscores (_) to make it italics\n - Use a pair of double-asterisks (**) on either side of some text, or a pair of double-underscores (__),  to make it bold\n - Enclose some text in a pair of double-backticks (``) to make it strike-through\n - Enclose a piece of code in a pair of backticks (`) to make it clearly visible on the page \n - Enclose a block of code in a pair of triple-backticks (```) to make it stand out from the rest of the page\n - Use a right angled-bracket (>) to create a blockquote \n - Use a dash (-) to create a list\n - For a numbered list, simply append 1. at the start of every list item instead of a dash (-)\n - Links are created using square brackets with the anchor tag e.g. [some link tag] followed by the URL enclosed in a pair of parentheses, e.g., (https://www.example.com)\n - Images follow a format similar to links but include a semicolon before the square brackets, for example, ![my image]. The text inside the square brackets becomes the "alt" tag'
+  "# Heading 1\n## Heading 2\n### Heading 3\n#### And so on...\n##### Just change the No. of hash (#) symbols\n\nHere is an _Italicized text_\n\nAnd some **bold text** just because why not! This __also works__\n\nAnd oh yeah!! We can also ~~strikethrough some text~~\n\nWe can have some code: `<div>Hello</div>`\n\nA multi-line code is also an option:\n```\nconst handleClick = () => {\n\tconsole.log('clicked');\n};\n\n```\nThis is how I would [link](https://www.facebook.com/kevin.njoroge) to my Facebook page\n\nIf you are like me, an inspiring quote can uplift a dull moment. Like this one I have picked from my [Quotes App](https://chipper-bubblegum-fb43cd.netlify.app):\n\n> \"Whether you think you can or think you can't, you're right.\" - Henry Ford.\n\nVisit the [app](https://chipper-bubblegum-fb43cd.netlify.app) to enjoy some more quotes\n\nAnd lastly, before I forgot, this page is almost getting boring for its lack of an image.\n\nSo, here is one:\n\n![Sample image](https://picsum.photos/id/1024/960/640)\n\n## In Summary\n\nLet's end with a recap of what you can do in the 'Editor' section to see real-time results in the 'Preview' section:\n - Use # to define headings\n - Enclose some text in a pair of underscores (_) to make it italics\n - Use a pair of double-asterisks (**) on either side of some text, or a pair of double-underscores (__),  to make it bold\n - Enclose some text in a pair of double-backticks (``) to make it strike-through\n - Enclose a piece of code in a pair of backticks (`) to make it clearly visible on the page \n - Enclose a block of code in a pair of triple-backticks (```) to make it stand out from the rest of the page\n - Use a right angled-bracket (>) to create a blockquote \n - Use a dash (-) to create a list\n - For a numbered list, simply append 1. at the start of every list item instead of a dash (-)\n - Links are created using square brackets with the anchor tag e.g. [some link tag] followed by the URL enclosed in a pair of parentheses, e.g., (https://www.example.com)\n - Images follow a format similar to links but include a semicolon before the square brackets, for example, ![my image]. The text inside the square brackets becomes the \"alt\" tag"
 
-  const App = () => {
+const App = () => {
   const [input, setInput] = useState(defaultInput)
   const output = DOMPurify.sanitize(marked.parse(input))
   const [isEditorVisible, setIsEditorVisible] = useState(true)
@@ -41,7 +41,7 @@ const defaultInput =
 
   useEffect(() => {
     document.getElementById('editor').style.minHeight = editorMinHeight
-    document.getElementById('preview').style.minHeight = previewerMinHeight 
+    document.getElementById('preview').style.minHeight = previewerMinHeight
   }, [editorMinHeight, previewerMinHeight])
 
   const handleOnChange = (event) => {
@@ -57,7 +57,9 @@ const defaultInput =
       >
         <div id='editor-header'>
           <div>
-            <label htmlFor='editor'><FaEdit /> {' '} Editor</label>
+            <label htmlFor='editor'>
+              <FaEdit /> Editor
+            </label>
           </div>
           <div>
             <FaArrowsAlt
@@ -82,7 +84,9 @@ const defaultInput =
         className={!isPreviewerVisible ? 'hide-section' : ''}
       >
         <div id='preview-header'>
-          <div><FaEye /> {' '} Previewer</div>
+          <div>
+            <FaEye /> Previewer
+          </div>
           <div>
             <FaArrowsAlt
               style={{ cursor: 'pointer' }}
